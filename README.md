f978fcd6bf3bd82d399ee83bd72c88e08818ae0d
Christopher P. Maher <chris@mahercode.io>
2018-02-07 10:24:52 -0800

    First run

---
+++ README.md
@@ -0,0 +1,9 @@
+# Habitat package: hab_vault_test
+
+## Description
+
+Provide a brief description of the `hab_vault_test` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `hab_vault_test` plan
---
+++ habitat/plan.sh
@@ -0,0 +1,17 @@
+pkg_name=vault
+pkg_origin=defilan
+pkg_version="0.1.0"
+pkg_maintainer="Christopher P. Maher <chris@mahercode.io>"
+pkg_license=("MIT")
+pkg_deps=(core/vault)
+
+pkg_svc_user=root
+pkg_svc_group=root
+
+do_build() {
+  return 0
+}
+
+do_install(){
+  return 0
+}
